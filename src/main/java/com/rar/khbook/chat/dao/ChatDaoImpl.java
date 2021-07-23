7c28ba1addbd57ff6d45d46a31b69682d9432e60
whskdy <whskdy@naver.com>
2021-07-24 02:18:18 +0900

    chat stop

--- src/main/java/com/rar/khbook/chat/dao/ChatDaoImpl.java
+++ src/main/java/com/rar/khbook/chat/dao/ChatDaoImpl.java
@@ -2,11 +2,14 @@ package com.rar.khbook.chat.dao;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+@Repository
 public class ChatDaoImpl implements ChatDao {
 
 	@Override
 	public void add(Map<String, Object> param) {
 
+		
 	}
 
 }
